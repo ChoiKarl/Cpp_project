04cf2c45c314a2a21cea9092fd0a0b0c369f85f2
ChoiKarl <253440030@qq.com>
2020-04-11 17:56:28 +0800

    Initial Commit

---
+++ c++ project/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  c++ project
+//
+//  Created by 蔡浩浩 on 2020/4/11.
+//  Copyright © 2020 蔡浩浩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
